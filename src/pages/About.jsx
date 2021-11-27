c3a1c03330bd071e791c671a81412ae8f14f5350
gabrielbadarau <badaraugabriel95@gmail.com>
2021-11-27 22:14:24 +0200

    updating About and TermsAndConditions

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,7 +5,10 @@ function About() {
     return(
         <div>
             <Layout>
-                <h1>About</h1>
+                <div className="container">
+                    <h1 className="row justify-content-center">About</h1>
+                    <p className="mt-5 text-justify">&emsp;&emsp;&emsp;&emsp;Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Bibendum enim facilisis gravida neque convallis a cras semper auctor. Tincidunt ornare massa eget egestas purus viverra accumsan in. Id semper risus in hendrerit gravida rutrum. Amet justo donec enim diam vulputate ut pharetra sit. Adipiscing diam donec adipiscing tristique risus. Cursus vitae congue mauris rhoncus aenean vel. Urna id volutpat lacus laoreet non. Aliquam purus sit amet luctus venenatis. Porttitor massa id neque aliquam vestibulum morbi blandit cursus. Cursus euismod quis viverra nibh cras pulvinar mattis nunc sed. Hac habitasse platea dictumst quisque sagittis purus sit amet. Pellentesque nec nam aliquam sem. Eleifend mi in nulla posuere. Facilisis leo vel fringilla est ullamcorper. Vel eros donec ac odio tempor. Turpis massa sed elementum tempus egestas sed. Enim eu turpis egestas pretium aenean pharetra magna ac. Felis eget velit aliquet sagittis id consectetur. Adipiscing bibendum est ultricies integer quis.</p>
+                </div>
             </Layout>
         </div>
     );
--- src/pages/TermsAndConditions.jsx
+++ src/pages/TermsAndConditions.jsx
@@ -5,7 +5,12 @@ function TermsAndConditions() {
     return (
         <div>
             <Layout>
-                <h1>Terms and conditions</h1>
+                <div className="container">
+                    <h1 className="row justify-content-center">Terms and conditions</h1>
+                    <p className="mt-5 text-justify">&emsp;&emsp;&emsp;&emsp;Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tortor at auctor urna nunc. Et leo duis ut diam quam nulla porttitor massa id. Turpis tincidunt id aliquet risus feugiat in ante. Vulputate eu scelerisque felis imperdiet proin fermentum leo vel. Odio aenean sed adipiscing diam. Elementum curabitur vitae nunc sed velit dignissim sodales ut eu. Suspendisse potenti nullam ac tortor vitae purus faucibus ornare. Tempus imperdiet nulla malesuada pellentesque. Et ligula ullamcorper malesuada proin libero nunc consequat interdum varius. Urna neque viverra justo nec ultrices dui sapien. Amet luctus venenatis lectus magna fringilla. Ultrices dui sapien eget mi. Sed pulvinar proin gravida hendrerit lectus. Nisi quis eleifend quam adipiscing vitae proin sagittis. Vitae congue mauris rhoncus aenean vel elit scelerisque.</p>
+
+                    <p className="mt-5 text-justify">&emsp;&emsp;&emsp;&emsp;Ornare arcu dui vivamus arcu felis bibendum ut tristique. Vitae elementum curabitur vitae nunc. Praesent semper feugiat nibh sed pulvinar proin gravida hendrerit. Euismod lacinia at quis risus sed. Enim ut sem viverra aliquet eget sit amet. Diam ut venenatis tellus in. Amet consectetur adipiscing elit duis tristique sollicitudin nibh sit amet. Cursus sit amet dictum sit. Porttitor rhoncus dolor purus non enim. Dui id ornare arcu odio ut. Mattis vulputate enim nulla aliquet porttitor lacus luctus accumsan tortor. Pretium fusce id velit ut tortor pretium. Ut porttitor leo a diam sollicitudin. Tempor orci eu lobortis elementum nibh tellus molestie nunc. Eu mi bibendum neque egestas congue quisque egestas. Duis convallis convallis tellus id interdum velit laoreet. Diam volutpat commodo sed egestas egestas fringilla phasellus faucibus scelerisque. Pellentesque elit eget gravida cum. Sit amet est placerat in egestas erat imperdiet sed. Scelerisque purus semper eget duis at tellus at.</p>
+                </div>
             </Layout>
         </div>
     )
